5cf8bf74ccc1776fd8a7cfa5f9fd60c31d660aef
Melo088 <jcml29310@gmail.com>
2026-04-26 20:33:04 -0500

    feat: add autoscaling, bastion host and launch template CloudFormation template

--- infrastructure/scripts/deploy-all.sh
+++ infrastructure/scripts/deploy-all.sh
@@ -3,6 +3,8 @@ set -e
 
 echo "=== Desplegando todos los stacks ==="
 
+IAM_PROFILE=$(aws iam list-instance-profiles --query 'InstanceProfiles[0].InstanceProfileName' --output text)
+
 deploy() {
   local name=$1
   local template=$2
@@ -16,6 +18,8 @@ deploy() {
   aws cloudformation wait stack-create-complete --stack-name $name
   echo "$name listo"
 }
+
+deploy ecom-s3-artifacts infrastructure/cloudformation/00-s3-artifacts.yaml
 
 deploy ecom-vpc infrastructure/cloudformation/01-vpc.yaml
 
@@ -31,6 +35,18 @@ deploy ecom-alb infrastructure/cloudformation/04-alb.yaml \
   --parameters \
     ParameterKey=VpcStackName,ParameterValue=ecom-vpc \
     ParameterKey=SgStackName,ParameterValue=ecom-sg
+
+deploy ecom-asg infrastructure/cloudformation/05-autoscaling.yaml \
+  --parameters \
+    ParameterKey=KeyName,ParameterValue=ecom-keypair \
+    ParameterKey=S3BucketName,ParameterValue=ecom-artifacts-prod-$(aws sts get-caller-identity --query Account --output text) \
+    ParameterKey=S3JarPath,ParameterValue=backend/ecom-app.jar \
+    ParameterKey=VpcStackName,ParameterValue=ecom-vpc \
+    ParameterKey=SgStackName,ParameterValue=ecom-sg \
+    ParameterKey=RdsStackName,ParameterValue=ecom-rds \
+    ParameterKey=AlbStackName,ParameterValue=ecom-alb \
+    ParameterKey=DBPassword,ParameterValue=SuperPassword123! \
+    ParameterKey=IamInstanceProfile,ParameterValue=$IAM_PROFILE
 
 echo ""
 echo "=== Todo desplegado ==="
